abfc831af30c5ca9a293c166b5ed3e22b21bfe7e
Matias Ziliotto <matias.ziliotto@mercadolibre.com>
2022-01-06 14:37:53 -0300

    add sale: repository & service

---
+++ internal/sale/repository.go
@@ -0,0 +1,109 @@
+package sale
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/matias-ziliotto/HackthonGo/internal/domain"
+)
+
+var (
+	// Db queries & statements
+	GetSaleQuery       = "SELECT id, invoice_id, product_id, quantity FROM sales WHERE id = ?"
+	StoreSaleStatement = "INSERT INTO sales(invoice_id, product_id, quantity) VALUES(?, ?, ?)"
+
+	// Errors
+	ErrorSaleNotFound              = errors.New("sale not found")
+	ErrorSalePrepareStoreStatement = errors.New("can not prepare store statement")
+	ErrorSaleExecStoreStatement    = errors.New("error executing store statement")
+)
+
+type SaleRepository interface {
+	Get(ctx context.Context, id int) (domain.Sale, error)
+	Store(ctx context.Context, sale domain.Sale) (domain.Sale, error)
+	StoreBulk(ctx context.Context, sales []domain.Sale) ([]domain.Sale, error)
+}
+
+func NewSaleRepository(db *sql.DB) SaleRepository {
+	return &saleRepository{
+		db: db,
+	}
+}
+
+type saleRepository struct {
+	db *sql.DB
+}
+
+func (r *saleRepository) Get(ctx context.Context, id int) (domain.Sale, error) {
+	var sale domain.Sale
+	err := r.db.QueryRowContext(ctx, GetSaleQuery, id).Scan(&sale)
+
+	if err != nil {
+		return domain.Sale{}, ErrorSaleNotFound
+	}
+
+	return sale, nil
+}
+
+func (r *saleRepository) Store(ctx context.Context, sale domain.Sale) (domain.Sale, error) {
+	stmt, err := r.db.PrepareContext(ctx, StoreSaleStatement)
+
+	if err != nil {
+		return domain.Sale{}, ErrorSalePrepareStoreStatement
+	}
+
+	defer stmt.Close()
+
+	result, err := stmt.ExecContext(ctx, sale.Invoice_id, sale.Product_id, sale.Quantity)
+
+	if err != nil {
+		return domain.Sale{}, ErrorSaleExecStoreStatement
+	}
+
+	id, err := result.LastInsertId()
+
+	if err != nil {
+		return domain.Sale{}, err
+	}
+
+	sale.Id = int(id)
+
+	return sale, nil
+}
+
+func (r *saleRepository) StoreBulk(ctx context.Context, sales []domain.Sale) ([]domain.Sale, error) {
+	valueStrings := make([]string, 0, len(sales))
+	valueArgs := make([]interface{}, 0, len(sales)*3)
+
+	for _, sale := range sales {
+		valueStrings = append(valueStrings, "(?, ?, ?, ?)")
+		valueArgs = append(valueArgs, sale.Id)
+		valueArgs = append(valueArgs, sale.Invoice_id)
+		valueArgs = append(valueArgs, sale.Product_id)
+		valueArgs = append(valueArgs, sale.Quantity)
+	}
+
+	stmtString := fmt.Sprintf("INSERT INTO sales (id, invoice_id, product_id, quantity) VALUES %s", strings.Join(valueStrings, ","))
+	stmt, err := r.db.PrepareContext(ctx, stmtString)
+	if err != nil {
+		return nil, ErrorSalePrepareStoreStatement
+	}
+
+	defer stmt.Close()
+
+	result, err := r.db.Exec(stmtString, valueArgs...)
+
+	if err != nil {
+		return nil, ErrorSaleExecStoreStatement
+	}
+
+	_, err = result.RowsAffected()
+	if err != nil {
+		return nil, err
+	}
+
+	return sales, nil
+}
---
+++ internal/sale/service.go
@@ -0,0 +1,74 @@
+package sale
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/matias-ziliotto/HackthonGo/internal/domain"
+	"github.com/matias-ziliotto/HackthonGo/pkg/file"
+)
+
+var (
+	SaleTxtPath = "../../datos/sales.txt"
+)
+
+type SaleService interface {
+	Get(ctx context.Context, id int) (domain.Sale, error)
+	StoreBulk(ctx context.Context) ([]domain.Sale, error)
+}
+
+func NewSaleService(pr SaleRepository) SaleService {
+	return &saleService{
+		repository: pr,
+	}
+}
+
+type saleService struct {
+	repository SaleRepository
+}
+
+func (s *saleService) Get(ctx context.Context, id int) (domain.Sale, error) {
+	sale, err := s.repository.Get(ctx, id)
+
+	if err != nil {
+		return domain.Sale{}, err
+	}
+
+	return sale, nil
+}
+
+func (s *saleService) StoreBulk(ctx context.Context) ([]domain.Sale, error) {
+	data, err := file.ReadFile(SaleTxtPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var sales []domain.Sale
+
+	for _, line := range data {
+		id, errId := strconv.Atoi(line[0])
+		productId, errProductId := strconv.Atoi(line[1])
+		invoiceId, errInvoiceId := strconv.Atoi(line[2])
+		quantity, errQuantity := strconv.ParseFloat(line[3], 64)
+
+		if errId == nil && errInvoiceId == nil && errProductId == nil && errQuantity == nil {
+			saleAux := domain.Sale{
+				Id:         id,
+				Invoice_id: invoiceId,
+				Product_id: productId,
+				Quantity:   quantity,
+			}
+
+			sales = append(sales, saleAux)
+		}
+	}
+
+	salesSaved, err := s.repository.StoreBulk(ctx, sales)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return salesSaved, nil
+}
