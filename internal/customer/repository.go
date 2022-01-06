4198b43539d21e0981dc9e3eede7adf31c72f2f5
Matias Ziliotto <matias.ziliotto@mercadolibre.com>
2022-01-06 13:43:02 -0300

    add customer: repository & service

---
+++ internal/customer/repository.go
@@ -0,0 +1,109 @@
+package customer
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/matias-ziliotto/HackthonGo/internal/domain"
+)
+
+var (
+	// Db queries & statements
+	GetCustomerQuery       = "SELECT id, first_name, last_name, situation FROM customers WHERE id = ?"
+	StoreCustomerStatement = "INSERT INTO customers(first_name, last_name, situation) VALUES(?, ?, ?)"
+
+	// Errors
+	ErrorCustomerNotFound              = errors.New("customer not found")
+	ErrorCustomerPrepareStoreStatement = errors.New("can not prepare store statement")
+	ErrorCustomerExecStoreStatement    = errors.New("error executing store statement")
+)
+
+type CustomerRepository interface {
+	Get(ctx context.Context, id int) (domain.Customer, error)
+	Store(ctx context.Context, customer domain.Customer) (domain.Customer, error)
+	StoreBulk(ctx context.Context, customers []domain.Customer) ([]domain.Customer, error)
+}
+
+func NewCustomerRepository(db *sql.DB) CustomerRepository {
+	return &customerRepository{
+		db: db,
+	}
+}
+
+type customerRepository struct {
+	db *sql.DB
+}
+
+func (r *customerRepository) Get(ctx context.Context, id int) (domain.Customer, error) {
+	var customer domain.Customer
+	err := r.db.QueryRowContext(ctx, GetCustomerQuery, id).Scan(&customer)
+
+	if err != nil {
+		return domain.Customer{}, ErrorCustomerNotFound
+	}
+
+	return customer, nil
+}
+
+func (r *customerRepository) Store(ctx context.Context, customer domain.Customer) (domain.Customer, error) {
+	stmt, err := r.db.PrepareContext(ctx, StoreCustomerStatement)
+
+	if err != nil {
+		return domain.Customer{}, ErrorCustomerPrepareStoreStatement
+	}
+
+	defer stmt.Close()
+
+	result, err := stmt.ExecContext(ctx, customer.FirstName, customer.LastName, customer.Situation)
+
+	if err != nil {
+		return domain.Customer{}, ErrorCustomerExecStoreStatement
+	}
+
+	id, err := result.LastInsertId()
+
+	if err != nil {
+		return domain.Customer{}, err
+	}
+
+	customer.Id = int(id)
+
+	return customer, nil
+}
+
+func (r *customerRepository) StoreBulk(ctx context.Context, customers []domain.Customer) ([]domain.Customer, error) {
+	valueStrings := make([]string, 0, len(customers))
+	valueArgs := make([]interface{}, 0, len(customers)*3)
+
+	for _, customer := range customers {
+		valueStrings = append(valueStrings, "(?, ?, ?, ?)")
+		valueArgs = append(valueArgs, customer.Id)
+		valueArgs = append(valueArgs, customer.FirstName)
+		valueArgs = append(valueArgs, customer.LastName)
+		valueArgs = append(valueArgs, customer.Situation)
+	}
+
+	stmtString := fmt.Sprintf("INSERT INTO customers (id, first_name, last_name, situation) VALUES %s", strings.Join(valueStrings, ","))
+	stmt, err := r.db.PrepareContext(ctx, stmtString)
+	if err != nil {
+		return nil, ErrorCustomerPrepareStoreStatement
+	}
+
+	defer stmt.Close()
+
+	result, err := r.db.Exec(stmtString, valueArgs...)
+
+	if err != nil {
+		return nil, ErrorCustomerExecStoreStatement
+	}
+
+	_, err = result.RowsAffected()
+	if err != nil {
+		return nil, err
+	}
+
+	return customers, nil
+}
---
+++ internal/customer/service.go
@@ -0,0 +1,74 @@
+package customer
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/matias-ziliotto/HackthonGo/internal/domain"
+	"github.com/matias-ziliotto/HackthonGo/pkg/file"
+)
+
+var (
+	CustomerTxtPath = "../../datos/customers.txt"
+)
+
+type CustomerService interface {
+	Get(ctx context.Context, id int) (domain.Customer, error)
+	StoreBulk(ctx context.Context) ([]domain.Customer, error)
+}
+
+func NewCustomerService(pr CustomerRepository) CustomerService {
+	return &customerService{
+		repository: pr,
+	}
+}
+
+type customerService struct {
+	repository CustomerRepository
+}
+
+func (s *customerService) Get(ctx context.Context, id int) (domain.Customer, error) {
+	customer, err := s.repository.Get(ctx, id)
+
+	if err != nil {
+		return domain.Customer{}, err
+	}
+
+	return customer, nil
+}
+
+func (s *customerService) StoreBulk(ctx context.Context) ([]domain.Customer, error) {
+	data, err := file.ReadFile(CustomerTxtPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var customers []domain.Customer
+
+	for _, line := range data {
+		id, errId := strconv.Atoi(line[0])
+		firstName := line[1]
+		lastName := line[2]
+		situation := line[3]
+
+		if errId == nil {
+			customerAux := domain.Customer{
+				Id:        id,
+				FirstName: firstName,
+				LastName:  lastName,
+				Situation: situation,
+			}
+
+			customers = append(customers, customerAux)
+		}
+	}
+
+	customersSaved, err := s.repository.StoreBulk(ctx, customers)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return customersSaved, nil
+}
